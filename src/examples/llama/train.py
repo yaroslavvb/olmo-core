47391511a3905adbc098c98e0fd3f336ceea7902
Yaroslav Bulatov <yaroslavvb@gmail.com>
2025-02-20 15:38:30 -0800

    enable wandb

--- src/examples/llama/train.py
+++ src/examples/llama/train.py
@@ -134,7 +134,7 @@ def build_config(run_name: str, overrides: List[str]) -> ExperimentConfig:
             WandBCallback(
                 name=run_name,
                 cancel_check_interval=10,
-                enabled=False,  # change to true to enable
+                enabled=True,  # change to true to enable
             ),
         )
         .with_callback("config_saver", ConfigSaverCallback())
